77572c44b1ad5f3aa77bbc733a922505b4e187fd
tab01105 <tab01105@gmail.com>
2018-01-25 15:44:34 +0900

    ラッチの実装

--- src/main/scala/FutureTaskForLatch.scala
+++ src/main/scala/FutureTaskForLatch.scala
@@ -4,10 +4,11 @@ object FutureTaskForLatch extends App {
 
   val futureTasks = for (i <- 1 to 3)
     yield new FutureTask[Int](() => {
-      Thread.sleep(1000)
+      Thread.sleep(i*1000)
       println(s"FutureTask ${i} finished")
       i
     })
   futureTasks.foreach((f) => new Thread(f).start())
-
+  futureTasks.foreach((f) => {f.get()})
+  println("finished")
 }
